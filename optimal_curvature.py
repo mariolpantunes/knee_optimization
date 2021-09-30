16117a4be58a005ecde79000fbcb208d89812279
Mario Antunes <mariolpantunes@gmail.com>
2021-09-30 17:46:48 +0100

    Remove the diskcache methods

--- optimal_curvature.py
+++ optimal_curvature.py
@@ -16,7 +16,7 @@ import tempfile
 import argparse
 import statistics
 import numpy as np
-#import diskcache as dc
+
 
 from enum import Enum
 from matplotlib import pyplot
@@ -55,15 +55,12 @@ class Agglomeration(Enum):
 args = None
 traces = []
 expecteds = []
-#rdp_cache_dic = {}
-#cost_cache_dic = {}
-
 
 
 # joblib cache
 location = tempfile.gettempdir()
 memory = joblib.Memory(location, verbose=0)
-#cache = dc.Cache(location+'/diskCache/curvature')
+
 
 def compute_rdp(idx, r):
     trace = traces[idx]
@@ -72,11 +69,6 @@ def compute_rdp(idx, r):
 
 # RDP cache
 rdp_cache = memory.cache(compute_rdp)
-
-#def rdp_cache(idx, r):
-#    if (idx, r) not in cache:
-#        cache[(idx, r)] = compute_rdp(idx, r)
-#    return cache[(idx, r)]
 
 
 def knee_cost(idx, r, cs, ct):
@@ -113,10 +105,6 @@ def knee_cost(idx, r, cs, ct):
 
 # Cost cache
 knee_cost_cache = memory.cache(knee_cost)
-#def knee_cost_cache(idx, r, cs, ct):
-#    if (idx, r, cs, ct) not in cache:
-#        cache[(idx, r, cs, ct)] = knee_cost(idx, r, cs, ct)
-#    return cache[(idx, r, cs, ct)]
 
 
 def compute_knees_cost(r, cs, ct):
--- optimal_lmethod.py
+++ optimal_lmethod.py
@@ -10,13 +10,12 @@ __status__ = 'Development'
 import os
 import re
 import csv
-#import joblib
+import joblib
 import logging
 import tempfile
 import argparse
 import statistics
 import numpy as np
-import diskcache as dc
 
 from enum import Enum
 from matplotlib import pyplot
@@ -56,13 +55,11 @@ class Agglomeration(Enum):
 args = None
 traces = []
 expecteds = []
-#rdp_cache_dic = {}
-#cost_cache_dic = {}
 
 # joblib cache
 location = tempfile.gettempdir()
-#memory = joblib.Memory(location, verbose=0)
-cache = dc.Cache(location+'/diskCache/lmethod')
+memory = joblib.Memory(location, verbose=0)
+
 
 def compute_rdp(idx, r):
     trace = traces[idx]
@@ -70,11 +67,8 @@ def compute_rdp(idx, r):
 
 
 # RDP cache
-#rdp_cache = memory.cache(compute_rdp)
-def rdp_cache(idx, r):
-    if (idx, r) not in cache:
-        cache[(idx, r)] = compute_rdp(idx, r)
-    return cache[(idx, r)]
+rdp_cache = memory.cache(compute_rdp)
+
 
 def knee_cost(idx, r, cs, ct):
     trace = traces[idx]
@@ -108,11 +102,7 @@ def knee_cost(idx, r, cs, ct):
 
 
 # Cost cache
-#knee_cost_cache = memory.cache(knee_cost)
-def knee_cost_cache(idx, r, cs, ct):
-    if (idx, r, cs, ct) not in cache:
-        cache[(idx, r, cs, ct)] = knee_cost(idx, r, cs, ct)
-    return cache[(idx, r, cs, ct)]
+knee_cost_cache = memory.cache(knee_cost)
 
 
 def compute_knees_cost(r, cs, ct):
